7709ddfda3f23211ee9f93f631e6d742a14446d9
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-07-25 13:09:37 +0700

    menambahkan category DAL

---
+++ SampleWebBlog/DAL/CategoryDAL.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Configuration;
+using System.Data.SqlClient;
+using SampleWebBlog.Models;
+
+namespace SampleWebBlog.DAL
+{
+    public class CategoryDAL
+    {
+        private string connStr;
+        public CategoryDAL()
+        {
+            connStr = 
+                ConfigurationManager
+                .ConnectionStrings["BootcampDbConnectionString"].ConnectionString;
+        }
+
+        public List<Category> GetAll()
+        {
+            using(SqlConnection conn = new SqlConnection(connStr))
+            {
+                List<Category> lstCat = new List<Category>();
+                string strSql = @"select * from Categories order by CategoryName asc";
+                SqlCommand cmd = new SqlCommand(strSql,conn);
+
+                conn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        lstCat.Add(
+                            new Category
+                            {
+                                CategoryId = Convert.ToInt32(dr["CategoryId"]),
+                                CategoryName = dr["CategoryName"].ToString()
+                            });
+                    }
+                }
+                dr.Close();
+                cmd.Dispose();
+                conn.Close();
+
+                return lstCat;
+            }
+        }
+    }
+}
